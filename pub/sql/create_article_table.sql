abc3b03d3c45dd4e30d75c711f76a656bb01d98c
Yoshifumi-T <2g.4423@gmail.com>
2020-05-22 03:54:16 +0900

    テーブル追加

--- pub/sql/create_article_table.sql
+++ pub/sql/create_article_table.sql
@@ -20,15 +20,15 @@ CREATE TABLE article_image_table (
         ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
--- CREATE TABLE article_text (
---     text_id      int UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
---     article_id   int UNSIGNED NOT NULL,
---     article_text text,
---     create_time  datetime,
---     update_time  datetime,
---     is_delete    bool,
---     CONSTRAINT fk_article_text
---     FOREIGN KEY (article_id) 
---         REFERENCES article (article_id)
---         ON DELETE RESTRICT ON UPDATE RESTRICT
--- );
+CREATE TABLE article_tag_table (
+    tag_id      int UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    article_id  int UNSIGNED NOT NULL,
+    article_tag varchar(100),
+    create_time datetime,
+    update_time datetime,
+    is_delete   bool,
+    CONSTRAINT fk_article_id_tag
+        FOREIGN KEY (article_id) 
+        REFERENCES article_table (article_id)
+        ON DELETE RESTRICT ON UPDATE RESTRICT
+);
--- pub/sql/insert_article_data.sql
+++ pub/sql/insert_article_data.sql
@@ -6,6 +6,6 @@ insert into article_image_table (article_id, article_image, create_time, update_
 insert into article_image_table (article_id, article_image, create_time, update_time, is_delete) values (1, 'image2', NOW(), NOW(), 0);
 insert into article_image_table (article_id, article_image, create_time, update_time, is_delete) values (1, 'image3', NOW(), NOW(), 0);
 
--- insert into article_text (article_id, article_text, create_time, update_time, is_delete) values (4, 'text1', NOW(), NOW(), 0);
--- insert into article_text (article_id, article_text, create_time, update_time, is_delete) values (4, 'text2', NOW(), NOW(), 0);
--- insert into article_text (article_id, article_text, create_time, update_time, is_delete) values (4, 'text3', NOW(), NOW(), 0);
+insert into article_tag_table (article_id, article_tag, create_time, update_time, is_delete) values (1, 'image1', NOW(), NOW(), 0);
+insert into article_tag_table (article_id, article_tag, create_time, update_time, is_delete) values (1, 'image2', NOW(), NOW(), 0);
+insert into article_tag_table (article_id, article_tag, create_time, update_time, is_delete) values (1, 'image3', NOW(), NOW(), 0);
