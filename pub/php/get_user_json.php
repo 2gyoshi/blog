0aedbfb802119be3636bb1c6fa2d5795326af93d
Yoshifumi-T <2g.4423@gmail.com>
2020-05-24 20:07:02 +0900

    コードリファクタリング

--- pub/php/get_user_json.php
+++ pub/php/get_user_json.php
@@ -1,6 +1,8 @@
 <?php
 
-require('./select.php');
+require('./utility.php');
+
+main();
 
 class User {
     public string $id = "";
@@ -15,21 +17,63 @@ class User {
     }
 }
 
-$id = "id";
-$password = "password";
-$file = "../sql/select_user.sql";
-
-$list = array();
-$sql = file_get_contents($file);
-$stmt = select($sql);
-
-while ($row = $stmt->fetch()) {
-    $user = new User();
-    $user->setID($row[$id]);
-    $user->setPassword($row[$password]);
-    array_push($list, $user);
+function main() {
+    echo get_user_json();
 }
 
-echo json_encode($list);
+function get_user_json() {
+    $result = array();
+    try {
+        $col_id = "id";
+        $col_password = "password";
+
+        // // クエリを実行する
+        $stmt = get_user_data();
+        while ($row = $stmt->fetch()) {
+            $user = new User();
+            $user->setID($row[$col_id]);
+            $user->setPassword($row[$col_password]);
+            array_push($result, $user);
+        }
+    } catch(PDOException $e) {
+        echo json_encode(get_api_result_failure($e->getMessage()));
+    }
+
+    return json_encode($result);
+}
+
+function get_user_data() {
+    $result = null;
+    $pdo = null;
+
+    try {
+        // DBへ接続する
+        $host = "localhost";
+        $db = "blog";
+        $user = "root";
+        $pass = "root";
+        $pdo  = new PDO("mysql:host=$host; dbname=$db;", $user, $pass);
+
+        $sql = get_select_sql();
+
+		// // クエリを実行する
+        $result = $pdo->query($sql);
+
+    } catch(PDOException $e) {
+        echo json_encode(get_api_result_failure($e->getMessage()));
+    } finally {
+        // 接続を閉じる
+        $pdo = null;
+    }
+
+    return $result;
+}
+
+function get_select_sql() {
+    $file = get_select_sql_file_name("user");
+    $sql = get_sql_file_content($file);
+    return $sql;
+}
+
 
 ?>
